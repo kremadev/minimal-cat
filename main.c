d5fba70f0da7f2e6592c63938a05b81495edf655
RAMDDR5 <bugthemonke@gmail.com>
2026-09-19 10:45:22 +0200

    not so minimal anymore

--- main.c
+++ main.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        int c;
+        while ((c = getchar()) != EOF)
+            putchar(c);
+        return 0;
+    }
+
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
             switch (argv[i][1]) {
                 case 'h':
                     printf("Usage:\n\t%s [FILE] # Prints the contents of the file.\n\t%s -h # Prints this menu.\n", argv[0], argv[0]);
-                    break;
+                    return 0;
                 default:
-                    printf("Unkown flag, use -h for help menu.\n");
-                    break;
-            }
-        } else {
-            FILE *file = fopen(argv[i], "r");
-            if (file == NULL) {
-                printf("Invalid file name.\n");
-                return 1;
-            }
-            fseek(file, 0, SEEK_END);
-            long size = ftell(file);
-            rewind(file);
-            for (long j = 0; j < size; j++) {
-                printf("%c", fgetc(file));
+                    printf("Unknown flag, use -h for help menu.\n");
+                    return 1;
             }
         }
+
+        FILE *file = fopen(argv[i], "r");
+        if (file == NULL) {
+            printf("Invalid file name.\n");
+            return 1;
+        }
+
+        int c;
+        while ((c = fgetc(file)) != EOF)
+            putchar(c);
+        fclose(file);
     }
     return 0;
 }
